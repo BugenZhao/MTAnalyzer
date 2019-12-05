4b8a4d0e79d27f25104dbcd1de31a7d74ef9a7fd
BugenZhao <bugenzhao@qq.com>
2019-12-05 23:31:34 +0800

    Moved base.hpp

---
+++ utilities/base.hpp
@@ -0,0 +1,18 @@
+//
+// Created by Bugen Zhao on 2019/12/4.
+//
+
+#ifndef FINALPROJECT_BASE_HPP
+#define FINALPROJECT_BASE_HPP
+
+#include <QVector>
+
+using Adj=QVector<QVector<bool>>;
+using Record=QString;
+
+namespace BugenZhao {
+    const QString TIME_FORMAT = "yyyy-MM-dd hh:mm:ss";
+}
+
+
+#endif //FINALPROJECT_BASE_HPP
